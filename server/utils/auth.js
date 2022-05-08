0844cab3a0d345d2d37d16b758f7fffcf7581420
Haguermeister <cadethague70@gmail.com>
2022-05-08 17:17:54 -0400

    Heroku

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,28 +7,22 @@ module.exports = {
   authMiddleware: function ({ req }) {
     let token =
       req.body?.token || req.query?.token || req.headers?.authorization;
-
-    if (req.headers?.authorization) {
-      token = token.split(" ").pop().trim();
-    }
-
     if (!token) {
       return req;
     }
-
+    if (req.headers?.authorization) {
+      token = token.split(" ").pop().trim();
+    }
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
     }
-
     return req;
   },
-
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
